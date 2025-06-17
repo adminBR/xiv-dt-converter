9447eb9623dc1bdd758e1c30f3e28fd0802e532d
adminBR <luis.cardosodev@gmail.com>
2025-06-17 19:01:45 -0300

    Feature: github stars button on the footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -9,11 +9,34 @@ const Footer = () => {
           </p>
           <p className="text-xs text-gray-500">Shit ass game.</p>
         </div>
+
         <p className="text-xs text-gray-500">
           This is a fan-made tool not affiliated with Square Enix. Final Fantasy
           XIV and all related titles are registered trademarks of Square Enix
           Holdings Co., Ltd.
         </p>
+        <div className="flex gap-2 p-2 bg-gray-800 rounded-xl shadow items-center">
+          <a
+            href="https://github.com/adminBR/xiv-dt-converter"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="https://img.shields.io/github/stars/adminBR/xiv-dt-converter?style=social"
+              alt="GitHub stars"
+            />
+          </a>
+          <a
+            href="https://github.com/adminBR/xiv-dt-converter/commits/main"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="https://img.shields.io/github/commit-activity/m/adminBR/xiv-dt-converter"
+              alt="GitHub commits"
+            />
+          </a>
+        </div>
       </div>
     </footer>
   );
